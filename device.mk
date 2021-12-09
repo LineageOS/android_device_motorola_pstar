8699c269dbe1758f7e4c1715c118372a3dbbe308
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-09 01:24:55 +0100

    pstar: rootdir: Import init.device.rc to set permissions
    Change-Id: I79d8e38d0a4514b8b2fc63f19d8acdfadd7b14f2

--- device.mk
+++ device.mk
@@ -21,6 +21,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
+# Init
+PRODUCT_PACKAGES += \
+    init.device.rc
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service.st \
