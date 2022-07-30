aabbe9b5edc571e2dcc7b399c862de0ebeeae124
electimon <electimon@gmail.com>
2022-07-30 18:21:27 +0000

    pstar: Enable HighTouchPollingRate interface
    Signed-off-by: electimon <electimon@gmail.com>
    Change-Id: I2e02d81bb3d3eb420c90d973afa878348447488d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,16 +11,21 @@ DEVICE_PATH := device/motorola/pstar
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := pstar
 
+# HIDL
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/lineageos_pstar_defconfig
 
-# Power
-SOONG_CONFIG_MOTO_KONA_TOUCH_SINGLE_TAP_PATH := /sys/devices/platform/soc/a94000.i2c/i2c-2/2-0049/single_click
-
-TARGET_TAP_TO_WAKE_NODE := "/sys/devices/platform/soc/a94000.i2c/i2c-2/2-0049/double_click"
-
 # SEPolicy
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+
+# Touch/Power
+SOONG_CONFIG_NAMESPACES += MOTO_KONA_TOUCH
+SOONG_CONFIG_MOTO_KONA_TOUCH := SINGLE_TAP_PATH USE_TOUCH_POLLING_RATE
+SOONG_CONFIG_MOTO_KONA_TOUCH_USE_TOUCH_POLLING_RATE := true
+SOONG_CONFIG_MOTO_KONA_TOUCH_SINGLE_TAP_PATH := /sys/devices/platform/soc/a94000.i2c/i2c-2/2-0049/single_click
+TARGET_TAP_TO_WAKE_NODE := "/sys/devices/platform/soc/a94000.i2c/i2c-2/2-0049/double_click"
 
 # Inherit from the proprietary version
 -include vendor/motorola/pstar/BoardConfigVendor.mk
