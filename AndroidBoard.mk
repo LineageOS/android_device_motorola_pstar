26f50c7fcbe733b424e984b0450236f0f9434031
electimon <electimon@gmail.com>
2022-07-30 01:01:53 +0000

    pstar: Copy needed modules to recovery ramdisk
    Signed-off-by: electimon <electimon@gmail.com>
    Change-Id: Ic6205e516461e9a3fe0beecf3b903d43a8a69928

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -4,4 +4,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+RECOVERY_KMOD_TARGETS += \
+    bq2597x_mmi.ko \
+    mmi_relay.ko \
+    mmi_annotate.ko \
+    mmi_info.ko \
+    mmi_parallel_charger.ko \
+    mmi_parallel_charger_qc3p.ko \
+    sensors_class.ko \
+    stmicro_mmi.ko \
+    touchscreen_mmi.ko
+
 -include device/motorola/sm8250-common/AndroidBoardCommon.mk
