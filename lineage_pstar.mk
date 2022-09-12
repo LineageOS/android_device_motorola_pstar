58c629c083cec489aa87e44db6d481df53e434da
SGCMarkus <markusornik@gmail.com>
2022-09-12 10:18:57 +0200

    pstar: update build fingerprint/description from S1RAS32M.41-20-16-5
    Change-Id: I1f4951722a0f2003032ea69b50695db1efbb790b

--- lineage_pstar.mk
+++ lineage_pstar.mk
@@ -22,7 +22,7 @@ PRODUCT_MODEL := motorola edge 20 pro
 PRODUCT_MANUFACTURER := motorola
 
 # Build info
-BUILD_FINGERPRINT := "motorola/pstar_retcn/pstar:12/S1RN32.55-16-1/cd155e-548707:user/release-keys"
+BUILD_FINGERPRINT := "motorola/pstar_retail/pstar:12/S1RAS32M.41-20-16-5/429588:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=pstar_retail \
-    PRIVATE_BUILD_DESC="pstar_retcn-user 12 S1RN32.55-16-1 cd155e-548707 release-keys"
+    PRIVATE_BUILD_DESC="pstar_retail-user 12 S1RAS32.41-20-16-5 9601c-429588 release-keys"
