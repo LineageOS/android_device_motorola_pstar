43b7b2dc958f9371a915e1d6810091d0011c41f5
SGCMarkus <markusornik@gmail.com>
2023-12-14 20:50:19 +0100

    pstar: path postproc lib in device tree too
    depending on if you call extract utils with --only-common/target
    or not, it might or might not patch the file.
    
    Change-Id: I26efe155aa764915c41db2d04518de340b795b73

--- extract-files.sh
+++ extract-files.sh
@@ -11,6 +11,10 @@ function blob_fixup() {
         vendor/lib/libmot_chi_desktop_helper.so | vendor/lib64/libmot_chi_desktop_helper.so)
             grep -q "libgui_shim_vendor.so" "${2}" || "${PATCHELF}" --add-needed "libgui_shim_vendor.so" "${2}"
             ;;
+        vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
+            hexdump -ve '1/1 "%.2X"' "${2}" | sed "s/130A0094/1F2003D5/g" | xxd -r -p > "${EXTRACT_TMP_DIR}/${1##*/}"
+            mv "${EXTRACT_TMP_DIR}/${1##*/}" "${2}"
+            ;;
     esac
 }
 
