a9e7849ef641a92f6700fadc7940b68636e41ae2
dianlujitao <dianlujitao@lineageos.org>
2024-09-08 23:11:53 +0800

    pstar: move product inheritance to common
    Change-Id: I3e3671d47c268d4cbb82ad44fb737ccccbb41455

--- lineage_pstar.mk
+++ lineage_pstar.mk
@@ -4,22 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 # Inherit from pstar device
 $(call inherit-product, device/motorola/pstar/device.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_pstar
 PRODUCT_DEVICE := pstar
-PRODUCT_BRAND := motorola
 PRODUCT_MODEL := motorola edge 20 pro
-PRODUCT_MANUFACTURER := motorola
 
 PRODUCT_SYSTEM_NAME := pstar_retail
 
