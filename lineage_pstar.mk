186bb2cdcf8e120b063dfe9654dc5c1cd6494b91
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    pstar: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- lineage_pstar.mk
+++ lineage_pstar.mk
@@ -12,10 +12,9 @@ PRODUCT_NAME := lineage_pstar
 PRODUCT_DEVICE := pstar
 PRODUCT_MODEL := motorola edge 20 pro
 
-PRODUCT_SYSTEM_NAME := pstar_retail
-
 # Build info
-BUILD_FINGERPRINT := "motorola/pstar_retail/pstar:13/T1RAS33.55-15-16-5/d9def9-6b8134:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="pstar_retail-user 13 T1RAS33.55-15-16-5 d9def9-6b8134 release-keys" \
-    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+    BuildDesc="pstar_retail-user 13 T1RAS33.55-15-16-5 d9def9-6b8134 release-keys" \
+    BuildFingerprint=motorola/pstar_retail/pstar:13/T1RAS33.55-15-16-5/d9def9-6b8134:user/release-keys \
+    DeviceProduct=pstar_retail \
+    SystemName=pstar_retail
