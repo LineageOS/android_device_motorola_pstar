88586182d53ec4fbf83dcaded519a9b5853d5946
dianlujitao <dianlujitao@lineageos.org>
2025-08-10 21:32:10 +0800

    Revert "pstar: Copy default secure_element config"
    This reverts commit 6215a283a6c98f607ab16d4578365021976a3e80.
    
    Change-Id: I4d50ce66c269f40c0079c1aff15540efe2b77889

--- device.mk
+++ device.mk
@@ -44,8 +44,5 @@ PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.2-service.st \
     nfc_nci.st21nfc.default
 
-PRODUCT_COPY_FILES += \
-    hardware/st/secure_element/libese-hal-st.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libese-hal-st.conf
-
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/motorola/pstar/pstar-vendor.mk)
