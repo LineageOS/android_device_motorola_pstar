3fe70ef3735d82677f66bef2444c16a22ee27dce
LuK1337 <priv.luk@gmail.com>
2025-08-20 19:20:35 -0400

    pstar: Migrate to AIDL LiveDisplay HAL
    Change-Id: I4e8be836e94d442b40c63a1ac7dade3c9d9c784c

--- device.mk
+++ device.mk
@@ -34,6 +34,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.device.rc
 
+# LiveDisplay
+$(call soong_config_set,livedisplay_motorola,enable_ab,false)
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_ODM)/etc/media_profiles_V1_0.xml
